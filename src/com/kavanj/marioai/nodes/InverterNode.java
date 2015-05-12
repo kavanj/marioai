5cb0dd217378f149f4e0c60816c0d3efc10401ae
Joe Kavanagh <jkavanagh90@gmail.com>
2015-05-12 18:38:34 -0400

    Added DecoratorNode, and InverterNod which extends it

---
+++ src/com/kavanj/marioai/nodes/InverterNode.java
@@ -0,0 +1,31 @@
+package com.kavanj.marioai.nodes;
+
+/**
+ * Created by joe on 2015-05-12.
+ */
+public class InverterNode extends DecoratorNode
+{
+
+    /**
+     * Creates a new instance of {@link InverterNode} using {@code child} as it's child
+     *
+     * @param child
+     *         The {@link Node} that will be used as this {@link InverterNode}'s child
+     */
+    public InverterNode(Node child)
+    {
+        super(child);
+    }
+
+    /**
+     * Inverts the result of this {@link InverterNode}'s child.
+     *
+     * @return {@code true} if {@code child.run()} returns {@code false},
+     * {@code false} if {@code child.run()} returns {@code true}
+     */
+    @Override
+    public boolean run()
+    {
+        return !child.run();
+    }
+}
